7aeb2006a039ba19f121bb99b2c2175a5b308c55
Gökhan Güzelkokar <gguzelkokar.mdbf15@iste.edu.tr>
2019-02-28 10:32:30 +0300

    Update dme_train.py

--- dme_train.py
+++ dme_train.py
@@ -78,7 +78,7 @@ a = []
 for i in x:
     a.append((int(x[0].split()[]))
 print(int(x[0].split()[1]))"""
-"""fname = "kernel_size.txt"
+fname = "kernel_size.txt"
 single = []
 double = []
 
@@ -160,114 +160,7 @@ for t in content:
                 model2.pop()
 
             model2.pop()
-            model2.pop()"""
-fname = "kernel_size.txt"
-single = []
-double = []
-
-with open(fname) as f:
-    content = f.readlines()
-content = [x.strip() for x in content] 
-
-for i in range(len(content)):
-    content[i] = int(content[i])
-
-for i in content:
-    if i % 2 ==0:
-        double.append(i)
-
-    else:
-        single.append(i)
-conv_1 = 0
-conv_2 = 0
-conv_3 = 0
-conv_4 = 0
-#print(content)
-
-#save = save_database()
-a = []
-def conv_last(last_layer,conv_3):
-    if last_layer % 2 ==0:
-        for r in single:
-            conv_4 = r
-            model.add(Conv2D(filters=3,kernel_size=r,strides=1,activation='relu'))
-            model.add(MaxPool2D(pool_size=2))
-            model.summary()
-            last_layer = model.layers[7].output.shape[1]
-            model.pop()
-            model.pop()
-            val = (conv_1,conv_2,conv_3,conv_4,int(last_layer))
-            #save.save_models(val)
-            a.append(val)
-    else:
-        for r in double:
-            conv_4 = r
-            model.add(Conv2D(filters=3,kernel_size=r,strides=1,activation='relu'))
-            model.add(MaxPool2D(pool_size=2))
-            model.summary()
-            last_layer = model.layers[7].output.shape[1]
-            model.pop()
-            model.pop()
-            val = (conv_1,conv_2,conv_3,conv_4,int(last_layer))
-            #save.save_models(val)
-            a.append(val)
-
-def conv__3(pool):
-    if pool % 2 == 0:
-        for j in single:
-            conv_3 = j
-            model.add(Conv2D(filters=3,kernel_size=j,strides=1,activation='relu'))
-            model.add(MaxPool2D(pool_size=2))
-            #model.summary()
-            last_layer = model.layers[5].output.shape[1]
-            ##################
-            conv_last(last_layer,conv_3)
-            ##################
-            model.pop()
-            model.pop()      
-    else:
-        for j in double:
-            conv_3 = j
-            model.add(Conv2D(filters=3,kernel_size=j,strides=1,activation='relu'))
-            model.add(MaxPool2D(pool_size=2))
-            #model.summary()
-            last_layer = model.layers[5].output.shape[1]
-            conv_last(last_layer,conv_3)
-            model.pop()
-            model.pop() 
-
-for t in content:
-    conv_1 = t
-    if conv_1 % 2 == 0 :
-        model = Sequential()
-        model.add(Conv2D(filters=3,kernel_size=t,strides=1,activation='relu',input_shape=(227, 227, 3)))
-        model.add(MaxPool2D(pool_size=2))
-        out_1 = model.output_shape[1]
-        if out_1 % 2 ==0:
-            for i in single:
-                model.add(Conv2D(filters=3,kernel_size=i,strides=1,activation='relu'))
-                model.add(MaxPool2D(pool_size=2))
-                pool = model.output_shape[1]
-                conv_2 = i
-                conv__3(pool)
-                model.pop()
-                model.pop()
-
-            model.pop()
-            model.pop()
-
-        else:
-            for i in double:
-                model.add(Conv2D(filters=3,kernel_size=i,strides=1,activation='relu'))
-                model.add(MaxPool2D(pool_size=2))
-                pool = model.output_shape[1]
-                conv_2 = i
-                conv__3(pool)
-                model.pop()
-                model.pop()
-
-            model.pop()
-            model.pop()
+            model2.pop()
 
 K.clear_session()
 mst_acc = 0
